84e2565c1f166a638f8f059fd5e083c978d08a30
dmalataeva <deniza.malataeva@gmail.com>
2017-05-06 18:00:49 -0400

    Buttons for main menu

---
+++ app/src/main/java/dmalataeva/brainiac/BrainiacActivity.java
@@ -0,0 +1,17 @@
+package dmalataeva.brainiac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class BrainiacActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_brainiac);
+    }
+
+
+}
