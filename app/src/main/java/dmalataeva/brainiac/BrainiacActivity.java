e59c907ff4cf3052d599fe1fc8fb31079649b84c
dmalataeva <deniza.malataeva@gmail.com>
2017-05-07 17:05:56 -0400

    Added button focus drawables and animation for logo

--- app/src/main/java/dmalataeva/brainiac/BrainiacActivity.java
+++ app/src/main/java/dmalataeva/brainiac/BrainiacActivity.java
@@ -1,16 +1,67 @@
 package dmalataeva.brainiac;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class BrainiacActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Context myContext = getApplicationContext();
         setContentView(R.layout.activity_brainiac);
+
+        addLogo(myContext);
+        addButtonPlay();
+        addButtonHighscores();
+        addButtonExit();
+    }
+
+    public void addLogo(Context cont) {
+        ImageView logo = (ImageView)findViewById(R.id.logo);
+        AnimatorSet logoSet = (AnimatorSet) AnimatorInflater.loadAnimator(cont, R.animator.animation_logo);
+        logoSet.setTarget(logo);
+        logoSet.start();
+    }
+
+    public void addButtonPlay() {
+        ImageButton play = (ImageButton)findViewById(R.id.play);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Toast.makeText(getApplicationContext(), "PLAY is clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void addButtonHighscores() {
+        ImageButton highscores = (ImageButton)findViewById(R.id.highscores);
+        highscores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Toast.makeText(getApplicationContext(), "HS is clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void addButtonExit() {
+        ImageButton exit = (ImageButton)findViewById(R.id.exit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Toast.makeText(getApplicationContext(), "EXIT is clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
